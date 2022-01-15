01c21c54b0ade23280f686c3b43309355e99a9da
graphieros <alec.lloyd.probert@gmail.com>
2022-01-15 18:21:50 +0100

    cleans up comments and logs

--- src/App.tsx
+++ src/App.tsx
@@ -32,28 +32,7 @@ function App() {
     stack = [];
   };
 
-  // const numClickHandler = (e: ClickEvent): void => {
-  //   e.preventDefault();
-  //   let value = e.target?.dataset.value;
-  //   let type = e.target?.dataset.type;
-
-  //   if (calc.num.toString().length < 16) {
-  //     setCalc({
-  //       ...calc,
-  //       num:
-  //         calc.num === 0 && value === "0"
-  //           ? "0"
-  //           : Number(calc.num) % 1 === 0
-  //           ? Number(calc.num + value)
-  //           : calc.num + value,
-  //       result: !calc.sign ? 0 : calc.result,
-  //     });
-  //     stack.push({ value: Number(value), type: type });
-  //   }
-  // };
-
   const reverseSign = (): void => {
-    console.log({ stackFromReverse: stack });
     if (!stack.length) {
       return;
     }
@@ -73,49 +52,11 @@ function App() {
     stack = stack.slice(-3, -2);
   };
 
-  // const percentClickHandler = (): void => {
-  //   let num = calc.num ? parseFloat(calc.num.toString()) : 0;
-  //   let result = calc.result ? parseFloat(calc.result.toString()) : 0;
-
-  //   setCalc({
-  //     ...calc,
-  //     num: (num /= Math.pow(100, 1)),
-  //     result: (result /= Math.pow(100, 1)),
-  //     sign: "",
-  //   });
-  // };
-
-  // const equalsClickHandler = (): void => {
-  //   if (calc.sign && calc.num) {
-  //     const math = (a: number, b: number, sign: string) =>
-  //       sign === "+"
-  //         ? a + b
-  //         : sign === "-"
-  //         ? a - b
-  //         : sign === "X"
-  //         ? a * b
-  //         : a / b;
-
-  //     setCalc({
-  //       ...calc,
-  //       result:
-  //         calc.num === "0" && calc.sign === "/"
-  //           ? "Can't divide with 0"
-  //           : math(Number(calc.result), Number(calc.num), calc.sign),
-  //       sign: "",
-  //       num: 0,
-  //       memory: ["..."],
-  //     });
-  //   }
-  // };
-
   const computeFromStack = () => {
     let result = 0;
     let previousOperand = "+";
 
     const convertToResult = (previousOp: string, val: number | string) => {
-      console.log({ previousOp, val });
-
       switch (previousOp) {
         case "÷":
           return result / Number(val);
@@ -130,7 +71,6 @@ function App() {
     };
 
     stack.forEach((el) => {
-      console.log({ previousOperand, result });
       switch (el.type) {
         case "number":
           result = convertToResult(previousOperand, Number(el.value));
@@ -148,46 +88,7 @@ function App() {
       ...calc,
       result: result,
     });
-
-    console.log({ result });
   };
-
-  // const operandClickHandler = (e: ClickEvent): void => {
-  //   e.preventDefault();
-  //   const buttonVal = e.target?.dataset.value;
-
-  //   const memo = [...calc.memory, calc.num.toString(), buttonVal];
-
-  //   stack.push(buttonVal);
-
-  //   // const result = () => {
-  //   //   if (!calc.result && calc.num) {
-  //   //     return calc.num;
-  //   //   }
-  //   //   return calc.result;
-  //   // };
-
-  //   computeFromStack();
-
-  //   setCalc({
-  //     ...calc,
-  //     sign: buttonVal,
-  //     // result: result(),
-  //     num: 0,
-  //     previousNum: calc.num,
-  //     memory: [...memo],
-  //   });
-  // };
-
-  // const dotClickHandler = (e: ClickEvent): void => {
-  //   e.preventDefault();
-  //   const dot = e.target.innerHTML;
-
-  //   setCalc({
-  //     ...calc,
-  //     num: !calc.num.toString().includes(".") ? calc.num + dot : calc.num,
-  //   });
-  // };
 
   const renderHistory = (): string => {
     let res: (string | number)[] = [];
@@ -217,9 +118,6 @@ function App() {
     let value = e.target?.dataset.value || "";
     let type = e.target?.dataset.type;
 
-    // console.log({ value, type });
-    console.log({ stackComputed: stack });
-
     let lastStackRecord = stack[stack.length - 1] || {
       isPositive: true,
       value: "",
@@ -239,7 +137,6 @@ function App() {
       };
     }
 
-    console.log({ lastStackRecord, value });
     switch (type) {
       case "reverse":
         reverseSign();
@@ -273,10 +170,6 @@ function App() {
         if (isAnOperand) {
           stack.pop();
         }
-        setCalc({
-          ...calc,
-          sign: value,
-        });
 
         feedStack({
           isPositive: true,
@@ -285,23 +178,11 @@ function App() {
           isVisible: true,
         });
 
-        // console.log("HERE");
-
         break;
 
       default:
         break;
     }
-
-    // click digit
-    // save digit in memo
-
-    // click operand
-    // store operand
-    // if previous digit, compute with next digit
-
-    // click equal
-    // computeStack
   };
 
   return (
