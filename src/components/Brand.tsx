e05a7ddda26e644588e62518a3c3fc1c4c37f783
graphieros <alec.lloyd.probert@gmail.com>
2022-01-16 12:12:37 +0100

    finalized version

---
+++ src/components/Brand.tsx
@@ -0,0 +1,17 @@
+import "../styles/brand.css";
+
+const Brand = () => {
+  return (
+    <a
+      href="https://github.com/graphieros/calc"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <span className="alp-brand">
+        basiCalc <span>by Alec Lloyd Probert</span>
+      </span>
+    </a>
+  );
+};
+
+export default Brand;
