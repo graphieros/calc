e13add3dbf3e309e1408426c38ca8287f1eb7134
graphieros <alec.lloyd.probert@gmail.com>
2022-01-29 11:36:00 +0100

    adds a couple of tests (WIP)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { text } from "node:stream/consumers";
+import { stack, feedStack, getColorTheme } from "./App";
+import { Num } from "./types/types";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("feedStack pushes an element to the stack", () => {
+  let element: Num = {
+    isPositive: true,
+    value: 1,
+    type: "digit",
+    isVisible: true,
+  };
+  feedStack(element);
+  expect(stack.length).toBe(1);
+});
+
+test("getColorTheme should return blue if param is 'blue'", () => {
+  expect(getColorTheme("blue")).toBe("blue");
+});
+
+test("getColorTheme should return 'default' if fed any other param than 'blue'", () => {
+  expect(getColorTheme("random string")).toBe("default");
+  expect(getColorTheme("default")).toBe("default");
+  expect(getColorTheme("")).toBe("default");
 });
