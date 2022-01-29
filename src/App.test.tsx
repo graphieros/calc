f672be1df5e31584606a33bff75230c2b43b2088
graphieros <alec.lloyd.probert@gmail.com>
2022-01-29 16:30:46 +0100

    adds a test for computeFromStack method

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
-import { stack } from "./App";
-import { StackElement } from "./types/types";
+import { Stack, StackElement } from "./types/types";
 import { feedStack, getColorTheme } from "./functions/index";
+import { computeFromStack } from "./functions/index";
+
+let stack: Stack = [];
+
+beforeEach((): void => {
+  stack = [];
+});
 
 test("feedStack pushes an element to the stack", () => {
-  let stackElement: StackElement = {
+  const stackElement: StackElement = {
     isPositive: true,
     value: 1,
     type: "digit",
@@ -22,4 +28,30 @@ test("getColorTheme should return 'default' if fed any other param than 'blue'",
   expect(getColorTheme("random string")).toBe("default");
   expect(getColorTheme("default")).toBe("default");
   expect(getColorTheme("")).toBe("default");
+});
+
+// //write a test for computeFromStack that checks if the result is correct
+test("computeFromStack should return the correct result", () => {
+  let stackElement: StackElement = {
+    isPositive: true,
+    value: 1,
+    type: "number",
+    isVisible: true,
+  };
+  feedStack(stack, stackElement);
+  stackElement = {
+    isPositive: true,
+    value: "x",
+    type: "operand",
+    isVisible: true,
+  };
+  feedStack(stack, stackElement);
+  stackElement = {
+    isPositive: true,
+    value: "2",
+    type: "number",
+    isVisible: true,
+  };
+  feedStack(stack, stackElement);
+  expect(computeFromStack(stack)).toBe(2);
 });
